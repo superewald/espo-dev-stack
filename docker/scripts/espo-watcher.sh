6beb486886fa48840343cc46f89f8b35b64991c9
SuperEwald <code@superewald.net>
2022-07-28 20:41:49 +0200

    added watcher for directories requiring rebuild

--- docker/scripts/espo-watcher.sh
+++ docker/scripts/espo-watcher.sh
@@ -14,6 +14,8 @@ scriptDir="$(dirname "$0")"
 SOURCE=$1
 DESTINATION=$2
 
+echo "Started EspoCRM watcher on $SOURCE directory!"
+
 # watch for changes in SOURCE directory
 inotifywait -r -m "$SOURCE" -e create,close_write,move,delete |
     while read directory action file; do
@@ -22,4 +24,25 @@ inotifywait -r -m "$SOURCE" -e create,close_write,move,delete |
 
         # sync files to destination
         syncWatched $action $path $dest
+
+        if [[ "$directory" == *"application/Espo/Resources/"* ]]; then
+            # rebuild cache
+            echo "Clearing cache and rebuilding.. "
+            php "$SOURCE/bin/command clear-cache"
+            php "$SOURCE/bin/command rebuild"
+        elif [[ "$directory" == *"client/src/"* ]]; then
+            echo "Building frontend library.."
+            prevCwd=$(pwd)
+            cd "$SOURCE"
+            npx grunt espo-bundle
+            npx grunt prepare-lib-original
+            npx grunt uglify:bundle
+            cd "$prevCwd"
+        elif [[ "$directory" == *"frontend/less/"* ]]; then
+            # build frontend css
+            echo "Building frontend css.."
+            npx grunt less
+            npx grunt cssmin
+        fi
+
     done
